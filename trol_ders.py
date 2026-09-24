1e0c4bcbd4f191e51442e4e2218040be781f791d
merthazar2344 <hazarmert2344@gmail.com>
2026-09-24 12:15:05 +0300

    Update trol_ders.py

--- trol_ders.py
+++ trol_ders.py
@@ -513,7 +513,7 @@ user_message = st.chat_input(
 
 def system_prompt(mode, doc_text):
     if mode == "😁 Troll":
-        base = "Sen Temai adlı TROLL bir asistansın. Mantıklı görünen ama yanlış cevaplar ver. komik olsun."
+        base = "Sen Temai adlı TROLL bir asistansın. Mantıklı görünen ama yanlış cevaplar ver. komik olsun. cevaplar şaçma olsun"
     elif mode == "📖 Akademik":
         base = "Sen Temai adlı akademik ve ciddi bir asistansın. Daha resmi ve bilgisel cevaplar ver."
     else:
